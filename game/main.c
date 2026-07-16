4a528057abde9d05b82932a268c0fe9317624d2d
Edenas Pocius <edenas.pocius@gmail.com>
2026-07-16 09:34:41 +0300

    Fix Game Gear input and tile rendering toolchain

--- game/main.c
+++ game/main.c
@@ -2,46 +2,78 @@
 
 SMS_EMBED_SEGA_ROM_HEADER_16KB(0, 0);
 
+static const unsigned char square_tile[8] = {
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+};
+
 void main(void)
 {
-    unsigned int keys;
+    unsigned int keys = 0;
+    unsigned int pressed_keys;
     const unsigned char *status;
+    unsigned char square_x = 15;
+    unsigned char square_y = 15;
+    unsigned char previous_x;
+    unsigned char previous_y;
 
-    SMS_init();
-    SMS_displayOff();
-
-    SMS_VRAMmemset(0x0000, 0x00, 0x4000);
-    SMS_VRAMmemsetW(SMS_PNTAddress, 0x0000, 32 * 28 * 2);
+    SMS_VRAMmemsetW(0, 0x0000, 16384);
 
     GG_setBGPaletteColor(0, RGB(0, 0, 0));
     SMS_setBackdropColor(0);
     SMS_autoSetUpTextRenderer();
+    SMS_load1bppTiles(square_tile, 96, sizeof(square_tile), 0, 1);
 
-    SMS_printatXY(6, 7, "GearRay");
-    SMS_printatXY(2, 8, "Hello Game Gear");
-    SMS_printatXY(2, 10, "Input: idle");
+    SMS_printatXY(12, 7, "GearRay");
+    SMS_printatXY(8, 8, "Hello Game Gear");
+    SMS_printatXY(8, 10, "Input: idle");
+    SMS_setTileatXY(square_x, square_y, 96);
 
     while (1)
     {
         SMS_waitForVBlank();
         keys = SMS_getKeysStatus();
+        pressed_keys = SMS_getKeysPressed();
+        previous_x = square_x;
+        previous_y = square_y;
+
+        if ((pressed_keys & PORT_A_KEY_UP) && square_y > 12)
+            --square_y;
+        else if ((pressed_keys & PORT_A_KEY_DOWN) && square_y < 20)
+            ++square_y;
+        else if ((pressed_keys & PORT_A_KEY_LEFT) && square_x > 6)
+            --square_x;
+        else if ((pressed_keys & PORT_A_KEY_RIGHT) && square_x < 25)
+            ++square_x;
 
         if (keys & PORT_A_KEY_UP)
-            status = "Input: up";
+        {
+            status = "Input: up      ";
+        }
         else if (keys & PORT_A_KEY_DOWN)
-            status = "Input: down";
+        {
+            status = "Input: down    ";
+        }
         else if (keys & PORT_A_KEY_LEFT)
-            status = "Input: left";
+        {
+            status = "Input: left    ";
+        }
         else if (keys & PORT_A_KEY_RIGHT)
-            status = "Input: right";
+        {
+            status = "Input: right   ";
+        }
         else if (keys & PORT_A_KEY_1)
             status = "Input: button 1";
         else if (keys & PORT_A_KEY_2)
             status = "Input: button 2";
         else
-            status = "Input: idle";
+            status = "Input: idle    ";
 
-        SMS_VRAMmemsetW(XYtoADDR(2, 10), 0x0000, 15 * 2);
-        SMS_printatXY(2, 10, status);
+        SMS_printatXY(8, 10, status);
+
+        if (square_x != previous_x || square_y != previous_y)
+        {
+            SMS_setTileatXY(previous_x, previous_y, 0);
+            SMS_setTileatXY(square_x, square_y, 96);
+        }
     }
 }
