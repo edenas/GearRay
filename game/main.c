12d927e92cf8c6e9a9a382d2f69f59c8a206c5f9
Edenas Pocius <edenas.pocius@gmail.com>
2026-07-16 09:00:01 +0300

    Add Game Gear input test

--- game/main.c
+++ game/main.c
@@ -4,6 +4,9 @@ SMS_EMBED_SEGA_ROM_HEADER_16KB(0, 0);
 
 void main(void)
 {
+    unsigned int keys;
+    const unsigned char *status;
+
     SMS_init();
     SMS_displayOff();
 
@@ -16,8 +19,29 @@ void main(void)
 
     SMS_printatXY(6, 7, "GearRay");
     SMS_printatXY(2, 8, "Hello Game Gear");
+    SMS_printatXY(2, 10, "Input: idle");
 
     while (1)
     {
+        SMS_waitForVBlank();
+        keys = SMS_getKeysStatus();
+
+        if (keys & PORT_A_KEY_UP)
+            status = "Input: up";
+        else if (keys & PORT_A_KEY_DOWN)
+            status = "Input: down";
+        else if (keys & PORT_A_KEY_LEFT)
+            status = "Input: left";
+        else if (keys & PORT_A_KEY_RIGHT)
+            status = "Input: right";
+        else if (keys & PORT_A_KEY_1)
+            status = "Input: button 1";
+        else if (keys & PORT_A_KEY_2)
+            status = "Input: button 2";
+        else
+            status = "Input: idle";
+
+        SMS_VRAMmemsetW(XYtoADDR(2, 10), 0x0000, 15 * 2);
+        SMS_printatXY(2, 10, status);
     }
 }
