9dc9a6ef5fdc12a65fad3137123f96c5ba84af61
Edenas Pocius <edenas.pocius@gmail.com>
2026-07-16 08:55:47 +0300

    Initialize Game Gear video hardware

--- game/main.c
+++ game/main.c
@@ -5,6 +5,17 @@ SMS_EMBED_SEGA_ROM_HEADER_16KB(0, 0);
 void main(void)
 {
     SMS_init();
+    SMS_displayOff();
+
+    SMS_VRAMmemset(0x0000, 0x00, 0x4000);
+    SMS_VRAMmemsetW(SMS_PNTAddress, 0x0000, 32 * 28 * 2);
+
+    GG_setBGPaletteColor(0, RGB(0, 0, 0));
+    SMS_setBackdropColor(0);
+    SMS_autoSetUpTextRenderer();
+
+    SMS_printatXY(6, 7, "GearRay");
+    SMS_printatXY(2, 8, "Hello Game Gear");
 
     while (1)
     {
